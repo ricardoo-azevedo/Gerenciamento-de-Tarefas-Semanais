676151c345c0a59613ef7ce4001a1eef746d75e4
Ricardo Azevedo <ricardoazevedo1911@gmail.com>
2024-11-27 17:43:17 -0300

    inicialização back

---
+++ Gerenciador-Tarefas/src/main/java/br/com/ricardoo_azevedo/Gerenciador_Tarefas/GerenciadorTarefasApplication.java
@@ -0,0 +1,13 @@
+package br.com.ricardoo_azevedo.Gerenciador_Tarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorTarefasApplication.class, args);
+	}
+
+}
